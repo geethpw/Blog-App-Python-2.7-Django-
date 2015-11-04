64cf1b18d5f546f2228a615389c88449b4471c80
Geeth <jgpwick@live.com>
2015-11-04 17:12:26 +0530

    pagination finalized

--- meblog/views.py
+++ meblog/views.py
@@ -3,10 +3,23 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-
+from django.core.paginator import Paginator
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    paginator = Paginator(posts, 3)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        posts = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'meblog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
