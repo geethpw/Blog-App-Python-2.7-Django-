f80abce263b9b058742c10b653b46f58fc4299e0
Geeth <jgpwick@live.com>
2015-11-04 14:57:59 +0530

    add/edit/publish blog posts module completed

--- meblog/views.py
+++ meblog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -13,13 +14,13 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'meblog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('meblog.views.post_detail', pk=post.pk)
     else:
@@ -39,3 +40,17 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     	return render(request, 'meblog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'meblog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('meblog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('meblog.views.post_list')
